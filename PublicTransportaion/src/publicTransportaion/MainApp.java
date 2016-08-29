93d0262729dc8f068cff608db6de782486d722d6
JimMa0312 <JimMa0312@outlook.com>
2016-08-29 14:02:24 +0800

    框架修改

---
+++ PublicTransportaion/src/publicTransportaion/MainApp.java
@@ -0,0 +1,16 @@
+package publicTransportaion;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
