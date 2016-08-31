cd2822a5ff57938d52d037d10ea2a24c9136d27b
JimMa0312 <JimMa0312@outlook.com>
2016-08-31 20:22:48 +0800

    拓展Station模型，新建Bus模型类

---
+++ PublicTransportaion/src/publicTransportaion/model/Bus.java
@@ -0,0 +1,9 @@
+package publicTransportaion.model;
+
+public class Bus {
+
+	public Bus() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- PublicTransportaion/src/publicTransportaion/model/Station.java
+++ PublicTransportaion/src/publicTransportaion/model/Station.java
@@ -21,5 +21,51 @@ public class Station {
 		this.stationAddress=new SimpleStringProperty(null);
 		this.stationGPS=new SimpleStringProperty(null);
 	}
+	
+	public void setStationName(String stationName) {
+		this.stationName.set(stationName);
+	}
+	
+	public String getStationName() {
+		return stationName.get();
+	}
+	
+	public StringProperty getStationNameProperty() {
+		return stationName;
+	}
+	
+	public void setStationID(String stationId) {
+		this.stationID.set(stationId);
+	}
 
+	public String getStationID() {
+		return stationID.get();
+	}
+	
+	public StringProperty getStationIDProperty() {
+		return stationID;
+	}
+	
+	public void setStationAddress(String stationAddress) {
+		this.stationAddress.set(stationAddress);
+	}
+	
+	public String getStationAddress() {
+		return stationAddress.get();
+	}
+	
+	public StringProperty getStationAddressProperty() {
+		return stationAddress;
+	}
+	
+	public void setStationGPS(String GPSValue) {
+		this.stationGPS.set(GPSValue);
+	}
+	public String getStationGPS() {
+		return stationGPS.get();
+	}
+	
+	public StringProperty getStationGPSProperty() {
+		return stationGPS;
+	}
 }
