124104e940beaf53f4144d71607f516280b37939
GuoGuo <GuoGuo@DELL5547>
2016-08-31 17:37:46 +0800

    管理页面以及新增班线窗口控制器

---
+++ PublicTransportaion/src/publicTransportaion/view/EditBusLineInformationController.java
@@ -0,0 +1,17 @@
+package publicTransportaion.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+
+public class EditBusLineInformationController {
+	@FXML
+	private TextField Bus_id;
+	@FXML
+	private TextField Time_lag;
+	@FXML
+	private HBox Time_start;
+	@FXML
+	private HBox Time_end;
+	
+}
---
+++ PublicTransportaion/src/publicTransportaion/view/TransationManageController.java
@@ -0,0 +1,59 @@
+package publicTransportaion.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class TransationManageController {
+	@FXML
+	private Label Bus_id;
+	@FXML
+	private Label Einge_id;
+	@FXML
+	private Label Frame_id;
+	@FXML
+	private Label Bus_type;
+	@FXML
+	private Label Bus_Chair;
+	@FXML
+	private Label Can_population;
+	//以上是车辆标签页的标签
+	@FXML
+	private Label Bus_No;
+	@FXML
+	private Label Start_point;
+	@FXML
+	private Label End_point;
+	@FXML
+	private Label Stream;
+	@FXML
+	private Label Time_start;
+	@FXML
+	private Label Time_End;
+	//以上是班线标签页面的标签
+	@FXML
+	private Label Station_Name;
+	@FXML
+	private Label Station_ID;
+	@FXML
+	private Label Station_Address;
+	//以上是站点标签页的标签
+	@FXML
+	private TextField neirong;
+	@FXML
+	private Button Search;
+	@FXML
+	private Label bus_id;		//车牌照号
+	@FXML
+	private Label bus_no;		//班线编号
+	@FXML
+	private Label start_time;	//每日发车时间
+	@FXML
+	private Label execute_date;	//执行日期
+	@FXML
+	private Label Use_Line; 	//线路使用
+	@FXML
+	private Label position_information; //位置信息
+	//
+}
