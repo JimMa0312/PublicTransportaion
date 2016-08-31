a06ebd06da18b8cd400ed63efe37f6d90884263c
JimMa0312 <JimMa0312@outlook.com>
2016-08-31 17:12:35 +0800

    更新Car模型

--- PublicTransportaion/src/publicTransportaion/model/Cars.java
+++ PublicTransportaion/src/publicTransportaion/model/Cars.java
@@ -6,12 +6,12 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class Cars {
-	private final StringProperty licensePlate;
-	private final StringProperty eingeId;
-	private final StringProperty frameId;
-	private final StringProperty busType;
-	private final IntegerProperty busChair;
-	private final IntegerProperty carPopulation;
+	private final StringProperty licensePlate;//车牌照号
+	private final StringProperty eingeId;//发动机号
+	private final StringProperty frameId;//车架编号
+	private final StringProperty busType;//车型
+	private final IntegerProperty busChair;//车辆座位
+	private final IntegerProperty carPopulation;//车辆核载人数
 
 	public Cars() {
 		// TODO Auto-generated constructor stub
@@ -76,5 +76,24 @@ public class Cars {
 	public void setBusChair(String busChair) {
 		this.busChair.set(Integer.parseInt(busChair));
 	}
+	
+	public int getBusChair() {
+		return busChair.get();
+	}
+	
+	public IntegerProperty getBusChairProperty() {
+		return busChair;
+	}
+	
+	public void setCarPopulation(String carPopulation) {
+		this.carPopulation.set(Integer.parseInt(carPopulation));
+	}
 
+	public int getCarPopulation() {
+		return carPopulation.get();
+	}
+	
+	public IntegerProperty getCarPopulationProperty() {
+		return carPopulation;
+	}
 }
