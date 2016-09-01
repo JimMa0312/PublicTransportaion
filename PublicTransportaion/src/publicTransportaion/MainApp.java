20503b2786783a0239145f7350ee2d2e9f8ad607
JimMa0312 <JimMa0312@outlook.com>
2016-09-01 11:26:10 +0800

    更新MainApp，增加图像渲染方法，允许测试

--- PublicTransportaion/src/publicTransportaion/MainApp.java
+++ PublicTransportaion/src/publicTransportaion/MainApp.java
@@ -1,13 +1,54 @@
 package publicTransportaion;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
+	
+	private Stage PrimaryStage;
+	private BorderPane rootLayout;
+	
 
 	@Override
 	public void start(Stage primaryStage) {
+		this.PrimaryStage=primaryStage;
+		this.PrimaryStage.setTitle("公交车查询系统");
 		
+		initRootLayout();
+		ShowBusInformationOverView();
+	}
+	
+	private void initRootLayout() {
+		try {
+			FXMLLoader loader=new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/OutLayer.fxml"));
+			rootLayout=(BorderPane) loader.load();
+			
+			Scene scene=new Scene(rootLayout);
+			PrimaryStage.setScene(scene);
+			PrimaryStage.show();
+		} catch (IOException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+	
+	private void ShowBusInformationOverView(){
+		try {
+			FXMLLoader loader=new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/ShowBusInforMation.fxml"));
+			AnchorPane busInformationDialog=(AnchorPane) loader.load();
+			
+			rootLayout.setCenter(busInformationDialog);
+		} catch (IOException e) {
+			// TODO: handle exception
+		}
 	}
 
 	public static void main(String[] args) {
