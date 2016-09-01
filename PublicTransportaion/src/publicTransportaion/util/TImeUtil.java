6a776b88c2f6c1ca46c9d531a7bd15bb914bd92b
JimMa0312 <JimMa0312@outlook.com>
2016-09-01 10:23:14 +0800

    新建 时间的LocalTime与String的格式转换类

---
+++ PublicTransportaion/src/publicTransportaion/util/TImeUtil.java
@@ -0,0 +1,27 @@
+package publicTransportaion.util;
+
+import java.time.DateTimeException;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class TImeUtil {
+	private static final String TIME_PATTER="HH:ss";
+	private static final DateTimeFormatter TIME_FORMATTER=DateTimeFormatter.ofPattern(TIME_PATTER);
+	
+	public static String format(LocalTime time) {
+		if (time==null) {
+			return null;
+		}
+		return TIME_FORMATTER.format(time);
+	}
+	
+	public static LocalTime parse(String TimeString) {
+		try {
+			return TIME_FORMATTER.parse(TimeString,LocalTime::from);
+		} catch (DateTimeException e) {
+			// TODO: handle exception
+			return null;
+		}
+	}
+
+}
