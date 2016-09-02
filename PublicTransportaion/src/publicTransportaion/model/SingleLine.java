d29b81d149e0ec3a1e5b50c82113d8550d16deb9
JimMa0312 <JimMa0312@outlook.com>
2016-09-02 09:09:20 +0800

    你们看看行不行

---
+++ PublicTransportaion/src/publicTransportaion/model/SingleLine.java
@@ -0,0 +1,20 @@
+package publicTransportaion.model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SingleLine {
+	private List<String> line;
+
+	public SingleLine() {
+		// TODO Auto-generated constructor stub
+		line=new ArrayList<String>();
+	}
+	
+	public SingleLine(String lienString){
+		String[] strings=lienString.split("|");
+		line= Arrays.asList(strings);
+	}
+
+}
