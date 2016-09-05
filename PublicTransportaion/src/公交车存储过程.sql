1d04db62161e26857d7935d6fa01a31ee0c6068a
JimMa0312 <JimMa0312@outlook.com>
2016-09-05 10:35:08 +0800

    增加公交车存储过程新建是的查重机制

--- PublicTransportaion/src/公交车存储过程.sql
+++ PublicTransportaion/src/公交车存储过程.sql
@@ -1,9 +1,58 @@
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('new_admin')
+            and   type = 'P')
+   drop procedure new_admin
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('new_bus')
+            and   type = 'P')
+   drop procedure new_bus
+go
+
+if exists (select 1
+			from sysobjects
+			where id=object_id('Car_information')
+			and type='P')
+	drop procedure Car_infomation
+go
+
+if exists (select 1
+			from sysobjects
+			where id=object_id('Map_information')
+			and type='P')
+	drop procedure Map_information
+go
+
+if exists (select 1
+			from sysobjects
+			where id=object_id('Route_Planning')
+			and type='P')
+	drop procedure Route_Planning
+go
+
+if exists (select 1
+			from sysobjects
+			where id=object_id('SID')
+			and type='P')
+	drop procedure SID
+go
+
+if exists (select 1
+			from sysobjects
+			where id=object_id('new_station')
+			and type='P')
+	drop procedure new_station
+go
+
 create procedure new_admin (@id char(10),@pwd varchar(50),@limit int) as insert into admin_information values(@id,@pwd,@limit)
 go			--管理员信息表插入记录的存储过程
 create procedure new_bus (@bus_no varchar(1),@time_s datetime,@time_e datetime,@time_l datetime) as insert into Bus_information values(@bus_no,@time_s,@time_e,@time_l)
 go			--班线信息表插入记录的存储过程
 create procedure new_car (@license_p varchar(9),@engine_id char(18),@frame_id char(10),@bus_t varchar(20),@can_p int,@bus_c int)
-as insert into Car_infomation values(@license_p,@engine_id,@frame_id,@bus_t,@can_p,@bus_c)
+as insert into Car_information values(@license_p,@engine_id,@frame_id,@bus_t,@can_p,@bus_c)
 go			--车辆信息插入
 create procedure new_mao(@station_id char(10),@sta_station char(10),@station_distance2 real,@station_state2 int)
 as insert into Map_information values(@station_id,@sta_station,@station_distance2,@station_state2)
