7cbdc5eefbc6d65a88e204bdbe29b7c44a15d9d6
Han-Wang <GuoGuo@DELL5547>
2016-09-08 09:44:49 +0800

    数据项属性微调

--- PublicTransportaion/src/公交车存储过程.sql
+++ PublicTransportaion/src/公交车存储过程.sql
@@ -52,7 +52,7 @@ go
 
 create procedure new_admin (@id char(10),@pwd varchar(50),@limit int) as insert into [dbo].[admin_information] values(@id,@pwd,@limit)
 go			--����Ա��Ϣ������¼�Ĵ洢����
-create procedure new_bus (@bus_no varchar(10),@time_s datetime,@time_e datetime,@time_l datetime) as insert into [dbo].[Bus_information] values(@bus_no,@time_s,@time_e,@time_l)
+create procedure new_bus (@bus_no varchar(10),@time_s time,@time_e time,@time_l time) as insert into [dbo].[Bus_information] values(@bus_no,@time_s,@time_e,@time_l)
 go			--������Ϣ������¼�Ĵ洢����
 create procedure new_car (@license_p varchar(9),@engine_id char(18),@frame_id char(10),@bus_t varchar(20),@can_p int,@bus_c int)
 as insert into [dbo].[Car_information] values(@license_p,@engine_id,@frame_id,@bus_t,@can_p,@bus_c)
@@ -63,7 +63,7 @@ go			--վ�����Ϣ�����
 create procedure new_plan (@bus_no varchar(10),@upstream text,@downstream text)
 as insert into [dbo].[Route_Planning] values(@bus_no,@upstream,@downstream)
 go			--��Ӽƻ��Ĵ洢����
-create procedure new_sid (@bus_no varchar(10),@station_id char(10),@license_p varchar(9),@engine_s datetime,@gps text,@carout_date datetime,@line_layer int)
+create procedure new_sid (@bus_no varchar(10),@station_id char(10),@license_p varchar(9),@engine_s time,@gps text,@carout_date date,@line_layer int)
 as insert into [dbo].[SID] values(@bus_no,@station_id,@license_p,@engine_s,@gps,@carout_date,@line_layer)
 go			--���SID�Ĵ洢����
 create procedure new_station (@staion_id char(10),@station_name varchar(10),@station_adress varchar(20),@station_gps text)
