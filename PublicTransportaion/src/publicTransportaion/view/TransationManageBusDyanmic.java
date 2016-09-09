a5b765de54517671d933b2a6c0a431132fe694ab
Han-Wang <GuoGuo@DELL5547>
2016-09-09 15:18:32 +0800

    发车计划界面

--- PublicTransportaion/src/publicTransportaion/view/TransationManageBusDyanmic.java
+++ PublicTransportaion/src/publicTransportaion/view/TransationManageBusDyanmic.java
@@ -3,12 +3,42 @@ package publicTransportaion.view;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import publicTransportaion.model.Bus;
+import publicTransportaion.model.Cars;
 
 public class TransationManageBusDyanmic implements ControlledStage, Initializable {
 	@SuppressWarnings("unused")
 	private StageController myController;
-
+	@FXML
+	private Label carPosIdLabel;
+	@FXML
+	private Label busIdLabel;
+	@FXML
+	private ChoiceBox<String> typeOfLineChoiceBox;
+	@FXML
+	private DatePicker WorkeDatePicker;
+	@FXML
+	private TextField hourTextField;
+	@FXML
+	private TextField minuteTextField;
+	@FXML
+	private TableView<Bus> busTableView;
+	@FXML
+	private TableColumn<Bus, String> BusTableBusIdColumn;
+	@FXML
+	private TableView<Cars> CarsTabelView;
+	@FXML
+	private TableColumn<Cars, String> CarsTableIdColumn;
+	@FXML
+	private TableColumn<Cars, Integer> carsTableChairColun;
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
