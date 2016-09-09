5dada89768d61de853c68d2eebd8aeb90a75a6b5
JimMa0312 <JimMa0312@outlook.com>
2016-09-09 11:27:16 +0800

    SingleLine安全问题修改

--- PublicTransportaion/src/publicTransportaion/util/SingleLine.java
+++ PublicTransportaion/src/publicTransportaion/util/SingleLine.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import publicTransportaion.model.Station;
+
 public class SingleLine {
+	private static final String PARSE=",";
 	private static List<String> line;
 	//以下方法可以将把String转List<String>类型
 	public static List<String> parse(String lineString){
 		line=new ArrayList<String>();
-		String[] strings=lineString.split("|");
+		String[] strings=lineString.split(PARSE);
 		line= Arrays.asList(strings);
 		return line;
 	}
@@ -19,7 +22,10 @@ public class SingleLine {
 		String str="";
 		for(int i=0;i<list.size();i++)
 		{
-			str+=list.get(i)+":";
+			str+=list.get(i);
+			if (i!=list.size()-1) {
+				str+=PARSE;
+			}
 		}
 		return str;
 	}
